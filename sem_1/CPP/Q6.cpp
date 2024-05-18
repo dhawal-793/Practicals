b8b975e17c0bc1e32a449a276bbdc99961f1b503
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-18 20:10:17 +0530

    Added Q6 ans for CPP Practical.

--- sem_1/CPP/Q6.cpp
+++ sem_1/CPP/Q6.cpp
@@ -1,11 +1,43 @@
-// Q6. Write a program which creates and uses array of object of a class.(implementing the list of Managers of accompany having details such as Name, Age etc....
+// Q6. Write a program which creates and uses array of object of a class.(implementing the list of Managers of a ccompany having details such as Name, Age etc....
 
 #include <iostream>
 
 using namespace std;
 
+class Manager
+{
+public:
+  string name;
+  int age;
+  string department;
+
+  Manager(string n, int a, string d)
+  {
+    name = n;
+    age = a;
+    department = d;
+  }
+
+  void display()
+  {
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Department: " << department << endl;
+  }
+};
+
 int main()
 {
+  Manager managerList[3] = {
+      Manager("John", 33, "Finance"),
+      Manager("Bob", 37, "HR"),
+      Manager("Martin", 29, "IT")};
+  cout << "Details of Managers:\n\n";
+  for (int i = 0; i < 3; i++)
+  {
+    managerList[i].display();
+    cout << endl;
+  }
 
   return 0;
 }
